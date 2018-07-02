dc95d82cd6d119dd76305185ce4d9bc7c7b8fcde
luy <luy@zparkhr.com.cn>
2018-07-02 21:10:10 +0800

    idea_git_app-dao 第一次提交，项目基础结构和基础代码

---
+++ app-dao/src/main/java/com/dao/HelloDao.java
@@ -0,0 +1,10 @@
+package com.dao;
+
+/**
+ * Created by Administrator on 2018/7/1 0001.
+ */
+public class HelloDao {
+    public String sayHello(String name){
+        return "name:"+name;
+    }
+}
